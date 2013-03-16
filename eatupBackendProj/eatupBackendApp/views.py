a68e43e8c83bfed8845be9766535b5cb7b4860c1
Leon Zhang <lwzhang@andrew.cmu.edu>
2013-03-16 13:20:07 -0400

    can now get events/users by IDs

--- eatupBackendProj/eatupBackendApp/views.py
+++ eatupBackendProj/eatupBackendApp/views.py
@@ -2,28 +2,39 @@ import os, re
 from django.conf import settings
 from django.http import (HttpResponse, HttpResponseBadRequest, 
                          HttpResponseServerError, HttpResponseForbidden, 
-                         HttpResponseRedirect)
+                         HttpResponseRedirect, HttpResponseNotFound)
 from eatupBackendApp.models import Event, AppUser, Location
 from eatupBackendApp.json_response import json_response
+from annoying.functions import get_object_or_None 
     
 @json_response()    
 def getUser(request):
     if 'uid' not in request.REQUEST:
-        return {'error': 'unable to retrieve user; no uid given'}
+        return {'error': 'missing id argument'}
     
-    allUsers = {}
-    for appUser in AppUser.objects.all():
-        allUsers[appUser.pk] = appUser.getDictForJson()
-    return allUsers
+    uid = request.REQUEST['uid']
+    if not uid.isdigit():
+        return {'error': 'invalid user'}
+        
+    foundUser = get_object_or_None(AppUser, uid=uid)
+    if foundUser is None:
+        return {'error': 'invalid user'}
+    else:
+        return foundUser.getDictForJson()
     
 @json_response()    
 def getEvent(request):
     if 'eid' not in request.REQUEST:
-        return {'error': 'unable to retrieve event; no eid given'}
+        return {'error': 'missing id argument'}
     
-    allEvents = {}
-    for event in Event.objects.all():
-        allEvents[event.pk] = event.getDictForJson()
-    return allEvents
+    eid = request.REQUEST['eid']
+    if not eid.isdigit():
+        return {'error': 'invalid event'}
+    
+    foundEvent = get_object_or_None(Event, eid=eid)
+    if foundEvent is None:
+        return {'error': 'invalid event'}
+    else:
+        return foundEvent.getDictForJson()
     
     
--- eatupBackendProj/readme.txt
+++ eatupBackendProj/readme.txt
@@ -1,4 +1,2 @@
 Django version: 1.4.5
-Tastypie version: 0.9.13
--- using Tastypie to provide RESTful api for app
--- see: http://django-tastypie.readthedocs.org/en/latest/
+django-annoying: https://github.com/skorokithakis/django-annoying
